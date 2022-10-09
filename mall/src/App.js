67163c1874b015fa6491f6761ce8f23bf4513e27
Dipaleed6 <95572756+Dipaleed6@users.noreply.github.com>
2022-10-09 15:55:32 +0530

    basic ecomall

---
+++ mall/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ecomall</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
